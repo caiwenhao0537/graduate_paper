73aeb914407cde77e12ec9a2eca0121aafd8adb1
caiwenhao0537 <caiwenhao0537@163.com>
2018-03-11 18:17:39 +0800

    3月11日更新

--- smart_wear_get_parameter.py
+++ smart_wear_get_parameter.py
@@ -11,11 +11,16 @@ parameter_dict={1:'移动物体的重量',2:'静止物体的重量',3:'移动物
 
 #工程参数代表性单词
 parameter_dict_list=[[],[],[],[],[],[],[],['space','size','volume','bulk','cubage'],['shape','form','appearance','outline','adumbration','lineament'],[['number','component'],['quantity','component'],['quantity','material']],[],[],[['durability','static'],['work','well','long'],['work','well','still']],[],[],[],['energy',['energy','consume','static']],['rate','power',['volume','power'],['internal','power'],['rate','work']],['pressure','stress','press','extrusion',['compressive','stress'],['extrusion','pressure']],[],['structural','structure','hyperstability','stability',['structural','stability']],[],['brightness','luminance','lighteness','light',['light','intensity']],['operating','operate','efficiency','efficient',['operat','efficiency']],['waste',['material','waste'],['solid','waste'],['liquid','waste']],[['time','waste'],['time','loss']],[['power','waste'],['waste','electricit']],[['retrieval','information'],'location',['determine','cause'],['know','status']],['noise','sound','strepitus'],[],[],['adapt',['wide','condition'],['variety','condition']],['compatibility','compatible','unite','associate','connect'],['comfort','convenient','convenience'],['reliability','dependability','reliable'],['repair','mend','overhaul','revamp'],['safe','safety','security','theft'],['injure','damage','breakdown'],['beautiful','artistic','appearance'],[],[['difficulty','assembly'],'install','build',['complexity','assembly']],[['accuracy','manufacturet']],['automatic','automation','automate'],[['additional','difficulty'],['additional','cost'],['production','cost'],['time','consume'],'expensive','prohibitive'],['complexity','complex','complicated','precision','precise'],[['complexity','control'],['difficulty','control']],['detect','identify','measure','measurement','survey','gauge','meter',['difficulty','detect'],['complexity','measure']],[['measurement','acuuracy'],['accuracy','survey']]]
-#获取正则表达式列表
+#获取工程参数正则表达式列表
 pattern_txt=open('D:\pythonproject\graduate_paper\pattern_0309.txt','r',encoding='utf-8')
 pattern_list=[]
 for line in pattern_txt:
     pattern_list.append(str(line.strip()))
+#获取工程参数改善或恶化关键词列表
+pattern_attribute_txt=open('D:\pydata\mypydata\paper_data\data0227\pattern_improve_worse.txt','r',encoding='utf-8_sig')
+pattern_attribute_list=[]
+for line in pattern_attribute_txt:
+    pattern_attribute_list.append(str(line.strip()))
 #抽取一篇专利中符合正则表达式的句子并返回列表
 def sentence_extract(patent_txt,patterns):#patent_txt为一篇专利的文本文档，patterns为正则表达式列表
     sentence_list=[]
@@ -69,9 +74,10 @@ def cos(vector1,vector2):
         return None
     else:
         return dot_product / ((normA*normB)**0.5)
+#加载模型
+model=g.Doc2Vec.load('D:\pydata\mypydata\paper_data\data1212\model2\model.bin')
 
-def words_similarity(word,parameter_words_list,model_dir,value):#计算单词word所属的工程参数
-    model=g.Doc2Vec.load(model_dir)
+def words_similarity(word,parameter_words_list,value):#计算单词word所属的工程参数
     parameter_num=0
     parameter_num_list=[]
     multi_parameter_list=[]
@@ -81,43 +87,59 @@ def words_similarity(word,parameter_words_list,model_dir,value):#计算单词wor
         #print('已完成%d个工程参数比对'%parameter_num)
         if len(parameter_words)==0:
             pass
-        for parameter in parameter_words:
-            if isinstance(parameter,str):
-                try:
-                    similarity=model.similarity(word,parameter)
-                    if similarity>=value or word==parameter:
-                        parameter_num_list.append(parameter_num)
-                        break
-                except:
-                    pass
-            else:
-                for i in parameter:
+        else:
+            for parameter in parameter_words:
+                if isinstance(parameter,str):
                     try:
-                        multi_parameter_list.append(word_vector_get(i,model))
-                        parameter_vector=vector_merge(multi_parameter_list)
-                        word_vector=word_vector_get(word,model)
-                        similarity=cos(parameter_vector,word_vector)
-                        if similarity>=value:
+                        similarity=model.similarity(word,parameter)
+                        if similarity>=value or word==parameter:
                             parameter_num_list.append(parameter_num)
                             break
                     except:
                         pass
+                else:
+                    for i in parameter:
+                        try:
+                            multi_parameter_list.append(word_vector_get(i,model))
+                            parameter_vector=vector_merge(multi_parameter_list)
+                            word_vector=word_vector_get(word,model)
+                            similarity=cos(parameter_vector,word_vector)
+                            if similarity>=value:
+                                parameter_num_list.append(parameter_num)
+                                break
+                        except:
+                            pass
     for j in set(parameter_num_list):
         parameter_list.append(parameter_dict[j])
     return parameter_list
+#抽取句子中含有的参数改善或恶化的单词
+def parameter_attribute_extract(sentence,pattern_list):
+    parameter_attribute_list=[]
+    for pattern_word in pattern_list:
+        p=re.compile(pattern_word,re.I)
+        for i in p.findall(sentence):
+            for j in i:
+                if j not in[' ',',','\.']:
+                    parameter_attribute_list.append(j)
+    return parameter_attribute_list
 
 def patent_parameter_extract(patent_sentence_list,parameter_dict_list):
     patent_parameter_list=[]
+    patent_parameter_attribute_list=[]
     for sentence in patent_sentence_list:
-        print(sentence)
+        sentence_parameter_list=[]
+        sentence_parameter_attribute_list=[]
         sentence_words_list=sentence.split()
         print(sentence_words_list)
         print(len(sentence_words_list))
         for word in sentence_words_list:
-            word_parameter=words_similarity(word,parameter_dict_list,'D:\pydata\mypydata\paper_data\data1212\model2\model.bin',0.8)
+            word_parameter=words_similarity(word,parameter_dict_list,0.8)
             if len(word_parameter)>0:
-                patent_parameter_list.append(word_parameter)
-    return patent_parameter_list
+                sentence_parameter_list.append(word_parameter)
+        patent_parameter_list.append(sentence_parameter_list)
+        sentence_parameter_attribute_list.append(parameter_attribute_extract(sentence,pattern_attribute_list))
+        patent_parameter_attribute_list.append(sentence_parameter_attribute_list)
+    return patent_parameter_list,patent_parameter_attribute_list
 
 
 
@@ -133,12 +155,15 @@ data=pd.read_excel('D:\pydata\mypydata\paper_data\data0227\smart_wear_0302.xlsx'
 patent_dict={}
 for i in range(1,2):
     patent_list=[]
+    patent_list3=[]
     patent_list2=[]
     patent_list.append(data.irow(i)['专利号'])
     sentence_list=sentence_extract(data.irow(i)['专利文本'],pattern_list)
     if len(sentence_list)>0:
         result=patent_parameter_extract(sentence_list,parameter_dict_list)
-        patent_list.append(result)
+        patent_list3.append(result[0])
+        patent_list3.append(result[1])
+    patent_list.append(patent_list3)
     patent_list2.append(patent_list)
     #print(patent_list2)
     patent_dict.update(dict(patent_list2))
--- test.py
+++ test.py
@@ -3,31 +3,21 @@ import pandas as pd
 import numpy as np
 import re
 
-def sentence_extract(patent_txt,patterns):#patent_txt为一篇专利的文本文档，patterns为正则表达式列表
-    sentence_list=[]
-    patent_sentence=patent_txt.split('.')
-    for sentence in patent_sentence:
-        for pattern_word in patterns:
-            p=re.compile(pattern_word)
-            data=p.findall(sentence.strip())
-            if len(data)>0:
-                for i in data:
-                    sentence_list.append(i)
-    return sentence_list
-pattern_txt=open('D:\pythonproject\graduate_paper\pattern_0309.txt','r',encoding='utf-8')
-pattern_list=[]
-for line in pattern_txt:
-    pattern_list.append(str(line.strip()))
-data=pd.read_excel('D:\pydata\mypydata\paper_data\data0227\smart_wear_0302.xlsx')
-patent_dict={}
-for i in range(1,9):
-    patent_list=[]
-    patent_list2=[]
-    patent_list.append(data.irow(i)['专利号'])
-    print(sentence_extract(data.irow(i)['专利文本'],pattern_list))
-    patent_list.append(sentence_extract(data.irow(i)['专利文本'],pattern_list))
-    patent_list2.append(patent_list)
-    #print(patent_list2)
-    patent_dict.update(dict(patent_list2))
-print(patent_dict)
+text='Hello,Are you ok? In my opinion,you areb very good!hi,arbyd boy.you must reduce your money.'
+pattern_attribute_txt=open('D:\pydata\mypydata\paper_data\data0227\pattern_improve_worse.txt','r',encoding='utf-8_sig')
+pattern_attribute_list=[]
+for line in pattern_attribute_txt:
+    pattern_attribute_list.append(str(line.strip()))
+
+
+def parameter_attribute_extract(sentence,pattern_list):
+    parameter_attribute_list=[]
+    for pattern_word in pattern_list:
+        p=re.compile(pattern_word,re.I)
+        for i in p.findall(sentence):
+            for j in i:
+                if j not in[' ',',','\.']:
+                    parameter_attribute_list.append(j)
+    return parameter_attribute_list
+print(parameter_attribute_extract(text,pattern_list))
 
