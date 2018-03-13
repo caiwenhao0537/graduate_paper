6a466caaf2c8d86564ed9facca22d54301e281da
caiwenhao0537 <caiwenhao0537@163.com>
2018-03-13 18:21:06 +0800

    3月13日更新

--- smart_wear_get_parameter.py
+++ smart_wear_get_parameter.py
@@ -4,11 +4,15 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame,Series
 import re
+import random
+from nltk.stem.wordnet import WordNetLemmatizer
+lem = WordNetLemmatizer()
 import nltk.stem
 s=nltk.stem.SnowballStemmer('english')
 #工程参数字典
 parameter_dict={1:'移动物体的重量',2:'静止物体的重量',3:'移动物体的长度',4:'静止物体的长度',5:'移动物体的面积',6:'静止物体的面积',7:'移动物体的体积',8:'静止物体的体积',9:'形状',10:'物质的质量',11:'信息（资料）的数量',12:'移动物体的耐久性',13:'静止物体的耐久性',14:'速度',15:'力',16:'移动物体消耗的能量',17:'静止物体消耗的能量',18:'功率',19:'张力/压力',20:'强度',21:'结构的稳定性',22:'温度',23:'明亮度',24:'运行效率',25:'物质的浪费',26:'时间的浪费',27:'能量的浪费',28:'信息的遗漏',29:'噪音',30:'有害的散发',31:'有害的副作用',32:'适应性',33:'兼容性或连通性',34:'使用方便性',35:'可靠性',36:'易修护性',37:'安全性',38:'易受伤性',39:'美观',40:'外来有害因素',41:'可制造性',42:'制造的准确度',43:'自动化程度',44:'生产率',45:'装置的复杂性',46:'控制的复杂性',47:'测量的难度',48:'测量的准确度'}
 
+attribute_result_list=['改善','恶化']
 #工程参数代表性单词
 parameter_dict_list=[[],[],[],[],[],[],[],['space','size','volume','bulk','cubage'],['shape','form','appearance','outline','adumbration','lineament'],[['number','component'],['quantity','component'],['quantity','material']],[],[],[['durability','static'],['work','well','long'],['work','well','still']],[],[],[],['energy',['energy','consume','static']],['rate','power',['volume','power'],['internal','power'],['rate','work']],['pressure','stress','press','extrusion',['compressive','stress'],['extrusion','pressure']],[],['structural','structure','hyperstability','stability',['structural','stability']],[],['brightness','luminance','lighteness','light',['light','intensity']],['operating','operate','efficiency','efficient',['operat','efficiency']],['waste',['material','waste'],['solid','waste'],['liquid','waste']],[['time','waste'],['time','loss']],[['power','waste'],['waste','electricit']],[['retrieval','information'],'location',['determine','cause'],['know','status']],['noise','sound','strepitus'],[],[],['adapt',['wide','condition'],['variety','condition']],['compatibility','compatible','unite','associate','connect'],['comfort','convenient','convenience'],['reliability','dependability','reliable'],['repair','mend','overhaul','revamp'],['safe','safety','security','theft'],['injure','damage','breakdown'],['beautiful','artistic','appearance'],[],[['difficulty','assembly'],'install','build',['complexity','assembly']],[['accuracy','manufacturet']],['automatic','automation','automate'],[['additional','difficulty'],['additional','cost'],['production','cost'],['time','consume'],'expensive','prohibitive'],['complexity','complex','complicated','precision','precise'],[['complexity','control'],['difficulty','control']],['detect','identify','measure','measurement','survey','gauge','meter',['difficulty','detect'],['complexity','measure']],[['measurement','acuuracy'],['accuracy','survey']]]
 #获取工程参数正则表达式列表
@@ -16,6 +20,51 @@ pattern_txt=open('D:\pythonproject\graduate_paper\pattern_0309.txt','r',encoding
 pattern_list=[]
 for line in pattern_txt:
     pattern_list.append(str(line.strip()))
+# 读取工程参数属性词数据
+parameter_list=['移动物体的重量','静止物体的重量','移动物体的长度','静止物体的长度','移动物体的面积','静止物体的面积','移动物体的体积','静止物体的体积','形状','物质的质量','信息（资料）的数量','移动物体的耐久性','静止物体的耐久性','速度','力','移动物体消耗的能量','静止物体消耗的能量','功率','张力/压力','强度','结构的稳定性','温度','明亮度','运行效率','物质的浪费','时间的浪费','能量的浪费','信息的遗漏','噪音','有害的散发','有害的副作用','适应性','兼容性或连通性','使用方便性','可靠性','易修护性','安全性','易受伤性','美观','外来有害因素','可制造性','制造的准确度','自动化程度','生产率','装置的复杂性','控制的复杂性','测量的难度','测量的准确度']
+parameter_attribute_words_data=open('D:\pydata\mypydata\paper_data\data0227\parameter_improve_worse.txt','r',encoding='utf-8')
+def parameter_and_attribute_dict(attibute_data):#获取工程参数及其对应的属性词词典
+    parameter_attribute_dit = {}
+    parameter_num = 0
+    for line in attibute_data:
+        parameter_attribute_list=[]
+        parameter_and_attribute_list=[]
+        list2=[]
+        for words in line.strip().split('&'):
+            parameter_attribute_list.append(words.split())
+        parameter_and_attribute_list.append(parameter_list[parameter_num])
+        parameter_and_attribute_list.append(parameter_attribute_list)
+        list2.append(parameter_and_attribute_list)
+        parameter_attribute_dit.update(dict(list2))
+        parameter_num+=1
+    return parameter_attribute_dit
+result_dict=parameter_and_attribute_dict(parameter_attribute_words_data)
+#判断参数的改善或恶化情况
+def parameter_attribute_judge(parameter_list,parameter_attribute_list):
+    while len(parameter_list) and len(parameter_attribute_list):
+        result=[]
+        attribute_word_list=[]
+        for attribute_word in parameter_attribute_list:
+            attribute_word_list.append(lem.lemmatize(attribute_word,"v"))
+        for parameter in parameter_list:
+            parameter_result=[]
+            parameter_improve_word_list=[]
+            parameter_worse_word_list=[]
+            for parameter_attribute_improve_word in result_dict[parameter][0]:
+                parameter_improve_word_list.append(lem.lemmatize(parameter_attribute_improve_word,"v"))
+            for parameter_attribute_worse_word in result_dict[parameter][1]:
+                parameter_worse_word_list.append(lem.lemmatize(parameter_attribute_worse_word,"v"))
+            for word in attribute_word_list:
+                if word in parameter_improve_word_list:
+                    parameter_result.append('%s改善'%parameter)
+                elif word in parameter_worse_word_list:
+                    parameter_result.append('%s恶化'%parameter)
+            if len(parameter_result)==0:
+                attribute_result=attribute_result_list[random.randint(0,1)]
+                parameter_result.append('%s%s'%(parameter,attribute_result))
+            for i in parameter_result:
+                result.append(i)
+        return result
 #获取工程参数改善或恶化关键词列表
 pattern_attribute_txt=open('D:\pydata\mypydata\paper_data\data0227\pattern_improve_worse.txt','r',encoding='utf-8_sig')
 pattern_attribute_list=[]
@@ -122,7 +171,7 @@ def parameter_attribute_extract(sentence,pattern_list):
                 if j not in[' ',',','\.']:
                     parameter_attribute_list.append(j)
     return parameter_attribute_list
-
+#抽取专利中的参数以及参数的改善或恶化情况
 def patent_parameter_extract(patent_sentence_list,parameter_dict_list):
     patent_parameter_list=[]
     patent_parameter_attribute_list=[]
@@ -139,7 +188,11 @@ def patent_parameter_extract(patent_sentence_list,parameter_dict_list):
         for parameter_attribute in parameter_attribute_extract(sentence,pattern_attribute_list):
             sentence_parameter_attribute_list.append(parameter_attribute)
         patent_parameter_attribute_list.append(list(set(sentence_parameter_attribute_list)))
-    return patent_parameter_list,patent_parameter_attribute_list
+        parameter_and_attribute_result=parameter_attribute_judge(list(set(sentence_parameter_list)),list(set(sentence_parameter_attribute_list)))
+        #print(patent_parameter_list)
+        #print(patent_parameter_attribute_list)
+        print(parameter_and_attribute_result)
+    return patent_parameter_list,patent_parameter_attribute_list,parameter_and_attribute_result
 
 
 
@@ -168,4 +221,4 @@ for i in range(1,3):
     #print(patent_list2)
     patent_dict.update(dict(patent_list2))
     print('已处理%d条专利'%i)
-print(patent_dict)
+#print(patent_dict)
--- test.py
+++ test.py
@@ -2,11 +2,63 @@
 import pandas as pd
 import numpy as np
 import re
-
+import random
 from nltk.stem.wordnet import WordNetLemmatizer
 lem = WordNetLemmatizer()
+#
+# word='conducive to'
+# print(lem.lemmatize(word,"v"))
 
-word='conducive to'
-print(lem.lemmatize(word,"v"))
+parameter_dict=['移动物体的重量','静止物体的重量','移动物体的长度','静止物体的长度','移动物体的面积','静止物体的面积','移动物体的体积','静止物体的体积','形状','物质的质量','信息（资料）的数量','移动物体的耐久性','静止物体的耐久性','速度','力','移动物体消耗的能量','静止物体消耗的能量','功率','张力/压力','强度','结构的稳定性','温度','明亮度','运行效率','物质的浪费','时间的浪费','能量的浪费','信息的遗漏','噪音','有害的散发','有害的副作用','适应性','兼容性或连通性','使用方便性','可靠性','易修护性','安全性','易受伤性','美观','外来有害因素','可制造性','制造的准确度','自动化程度','生产率','装置的复杂性','控制的复杂性','测量的难度','测量的准确度']
+
+data=open('D:\pydata\mypydata\paper_data\data0227\parameter_improve_worse.txt','r',encoding='utf-8')
+
+def parameter_and_attribute_dict(attibute_data):
+    parameter_attribute_dit = {}
+    parameter_num = 0
+    for line in attibute_data:
+        parameter_attribute_list=[]
+        parameter_and_attribute_list=[]
+        list2=[]
+        for words in line.strip().split('&'):
+            parameter_attribute_list.append(words.split())
+        parameter_and_attribute_list.append(parameter_dict[parameter_num])
+        parameter_and_attribute_list.append(parameter_attribute_list)
+        list2.append(parameter_and_attribute_list)
+        parameter_attribute_dit.update(dict(list2))
+        parameter_num+=1
+    return parameter_attribute_dit
+
+result_dict=parameter_and_attribute_dict(data)
+attribute_result_list=['改善','恶化']
+parameter_list=['安全性','装置的复杂性']
+parameter_attribute_list=['enhance']
+def parameter_attribute_judge(parameter_list,parameter_attribute_list):
+    result=[]
+    attribute_word_list=[]
+    for attribute_word in parameter_attribute_list:
+        attribute_word_list.append(lem.lemmatize(attribute_word,"v"))
+    for parameter in parameter_list:
+        parameter_improve_word_list=[]
+        parameter_worse_word_list=[]
+        for parameter_attribute_improve_word in result_dict[parameter][0]:
+            parameter_improve_word_list.append(lem.lemmatize(parameter_attribute_improve_word,"v"))
+        for parameter_attribute_worse_word in result_dict[parameter][1]:
+            parameter_worse_word_list.append(lem.lemmatize(parameter_attribute_worse_word,"v"))
+        for word in attribute_word_list:
+            if word in parameter_improve_word_list:
+                result.append('%s改善'%parameter)
+            elif word in parameter_worse_word_list:
+                    result.append('%s恶化'%parameter)
+        if len(result)==0:
+            attribute_result=attribute_result_list[random.randint(0,1)]
+            result.append('%s%s'%(parameter,attribute_result))
+    return result
+print(parameter_attribute_judge(parameter_list,parameter_attribute_list))
+
+
+
+
+
 
 
